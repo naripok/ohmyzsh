18a7923db6856f4ffbfdf17a1a54307877e8ae2b
naripok <fernando.canteruccio@gmail.com>
2019-12-31 12:22:06 -0300

    saves mode between lines

--- plugins/vi-mode-indicator/vi-mode-indicator.plugin.zsh
+++ plugins/vi-mode-indicator/vi-mode-indicator.plugin.zsh
@@ -1,8 +1,16 @@
+# BETTER VI MODE
+# STARTS AS COMMAND MODE, SAME AS VIM, MAINTAINS MODE BETWEEN LINES, AWESOME MODE INDICATOR
+# MAINTAINER: NARIPOK, https://github.com/naripok
+
+IS_INIT=1
+PREV_KEYMAP="main"
+
 # set vi insert mode at startup
 bindkey -v
 
 # faster vi mode switching
 export KEYTIMEOUT=1
+
 
 function zle-keymap-select() {
   # update keymap variable for the prompt
@@ -13,20 +21,36 @@ function zle-keymap-select() {
 }
 zle -N zle-keymap-select
 
+
+function zle-line-finish() {
+  PREV_KEYMAP=$VI_KEYMAP
+}
+zle -N zle-line-finish
+
+
 function zle-line-init() {
+  if [ $IS_INIT = 1 ]; then
+    IS_INIT=0
+    zle -K vicmd;
+  fi
+
+  if [ $PREV_KEYMAP = vicmd ]; then
+    zle -K vicmd;
+  fi
+
   zle zle-keymap-select
 }
 zle -N zle-line-init
 
-function vi-accept-line() {
-  VI_KEYMAP=main
-  zle accept-line
-}
-zle -N vi-accept-line
-
-# use custom accept-line widget to update $VI_KEYMAP
-bindkey -M vicmd '^J' vi-accept-line
-bindkey -M vicmd '^M' vi-accept-line
+# function vi-accept-line() {
+#   VI_KEYMAP=vicmd
+#   zle accept-line
+# }
+# zle -N vi-accept-line
+# 
+# # use custom accept-line widget to update $VI_KEYMAP
+# bindkey -M vicmd '^J' vi-accept-line
+# bindkey -M vicmd '^M' vi-accept-line
 
 # if mode indicator wasn't setup by theme, define default
 if [[ "$COMMAND_INDICATOR" == "" ]]; then
@@ -40,3 +64,8 @@ fi
 function vi_mode_prompt_info() {
   echo "${${VI_KEYMAP/vicmd/$COMMAND_INDICATOR}/(main|viins)/$INSERT_INDICATOR}"
 }
+
+# define right prompt, if it wasn't defined by a theme
+if [[ "$RPS1" == "" && "$RPROMPT" == "" ]]; then
+  RPS1='$(vi_mode_prompt_info)'
+fi
